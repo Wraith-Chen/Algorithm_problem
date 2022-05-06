8961ac40c0507034a82f9474c8dec36a2d80e9a1
Wraith <1363380539@qq.com>
2022-05-06 17:28:08 +0800

    2022/5/5

--- 模拟/leetcode_905.java
+++ 模拟/leetcode_905.java
@@ -5,10 +5,19 @@ package 模拟;
  * @Difficulty_Level easy
  */
 /*
-
+    简单模拟，双指针。
 */
 public class leetcode_905 {
     public int[] sortArrayByParity(int[] nums) {
-        
+        int n = nums.length;
+        for (int i = 0, j = n - 1; i < j; i++) {
+            if (nums[i] % 2 == 1) {
+                int c = nums[j];
+                nums[j--] = nums[i];
+                //原有位置i交换后不确保是偶数
+                nums[i--] = c;
+            }
+        }
+        return nums;
     }
 }
