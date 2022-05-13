39ea430d51b807479c9da2d3f121f51435c0ca52
Wraith <1363380539@qq.com>
2022-05-13 19:42:20 +0800

    2022/5/13

---
+++ 递归迭代/leetcode_22.java
@@ -0,0 +1,51 @@
+package 递归迭代;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Time 2022-05-13
+ * @Title leetcode_22 括号生成
+ * @Difficulty_Level middle
+ */
+/*
+    减枝回溯。
+*/
+public class leetcode_22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        // 特判
+        if (n == 0) {
+            return res;
+        }
+
+        dfs("", 0, 0, n, res);
+        return res;
+    }
+
+    /**
+     * @param curStr 当前递归得到的结果
+     * @param left   左括号已经用了几个
+     * @param right  右括号已经用了几个
+     * @param n      左括号、右括号一共得用几个
+     * @param res    结果集
+     */
+    private void dfs(String curStr, int left, int right, int n, List<String> res) {
+        if (left == n && right == n) {
+            res.add(curStr);
+            return;
+        }
+
+        // 剪枝
+        if (left < right) {
+            return;
+        }
+
+        if (left < n) {
+            dfs(curStr + "(", left + 1, right, n, res);
+        }
+        if (right < n) {
+            dfs(curStr + ")", left, right + 1, n, res);
+        }
+    }
+}
